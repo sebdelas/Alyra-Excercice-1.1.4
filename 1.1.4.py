6e1a72c043ca58dc71e52be6ea378bbbc2842bfc
Sébastien DELAS <47754201+sebdelas@users.noreply.github.com>
2019-02-18 18:35:17 +0100

    Add files via upload

---
+++ 1.1.4.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+from math import pi
+
+class Cercle:
+   
+   # Attribut
+   rayon = ""
+   
+   def __init__(self, rayon):
+     self.rayon = int(rayon)
+     
+   def air(self):
+      return pi * self.rayon ** 2
+   
+   def perimetre(self):
+      return 2 * pi * self.rayon
+
+
+rayon = saisie = input("Saisissez le rayon du cercle : ")
+cercle = Cercle(rayon)
+print ("Air : " + str(cercle.air()))
+print ("Perimetre : " + str(cercle.perimetre()))
